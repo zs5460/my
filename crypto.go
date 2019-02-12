3044574ded433adb9e7a5cc94cc1935b9a588b3a
zs5460 <zs5460@gmail.com>
2019-02-12 23:07:35 +0800

    modify crypto

--- crypto.go
+++ crypto.go
@@ -5,14 +5,12 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"encoding/hex"
-	"hash"
 	"io"
 )
 
 // MD5 ...
 func MD5(s string) (ret string) {
-	var h hash.Hash
-	h = md5.New()
+	h := md5.New()
 	io.WriteString(h, s)
 	ret = hex.EncodeToString(h.Sum(nil))
 	return
@@ -20,8 +18,7 @@ func MD5(s string) (ret string) {
 
 // SHA1 ...
 func SHA1(s string) (ret string) {
-	var h hash.Hash
-	h = sha1.New()
+	h := sha1.New()
 	io.WriteString(h, s)
 	ret = hex.EncodeToString(h.Sum(nil))
 	return
@@ -29,8 +26,7 @@ func SHA1(s string) (ret string) {
 
 // SHA256 ...
 func SHA256(s string) (ret string) {
-	var h hash.Hash
-	h = sha256.New()
+	h := sha256.New()
 	io.WriteString(h, s)
 	ret = hex.EncodeToString(h.Sum(nil))
 	return
