1c6f3331863ecf043b1ade86e1cdd18278ad8a3a
zs5460 <zs5460@gmail.com>
2019-02-22 15:06:45 +0800

    fix

--- datetime.go
+++ datetime.go
@@ -1,7 +1,6 @@
 package my
 
 import (
-	"strconv"
 	"time"
 )
 
@@ -13,19 +12,16 @@ func Now() string {
 // FriendlyTime ...
 func FriendlyTime(t time.Time) string {
 	seconds := int(time.Now().Sub(t).Seconds())
-
 	switch {
 	case seconds > 0 && seconds < 60:
-		return cstr(seconds) + "刚刚"
+		return "刚刚"
 	case seconds >= 60 && seconds < 3600:
-		return cstr(seconds/60) + "分钟前"
+		return CStr(seconds/60) + "分钟前"
 	case seconds >= 3600 && seconds < 86400:
-		return cstr(seconds/3600) + "小时前"
+		return CStr(seconds/3600) + "小时前"
+	case seconds >= 86400 && seconds < 604800:
+		return CStr(seconds/86400) + "天前"
 	default:
 		return t.Format("2006-01-02")
 	}
-}
-
-func cstr(i int) string {
-	return strconv.Itoa(i)
 }
