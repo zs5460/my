6606370282daf33e59104c26c747f6453befb991
zs5460 <zs5460@gmail.com>
2019-03-12 15:36:38 +0800

    update example

--- example_test.go
+++ example_test.go
@@ -6,6 +6,16 @@ import (
 	"github.com/zs5460/my"
 )
 
+func ExampleGetURL() {
+	s, err := my.GetURL("http://54600.net/demo/ping")
+	if err != nil {
+		return
+	}
+	fmt.Println(string(s))
+	// Output:
+	// PONG
+}
+
 func ExampleGetJSON() {
 	type Result struct {
 		Code    int           `json:"code"`
@@ -14,9 +24,22 @@ func ExampleGetJSON() {
 		Total   int           `json:"total"`
 	}
 	var ret Result
-	err := my.GetJSON("http://abc.com/api/.../", &ret)
+	err := my.GetJSON("http://54600.net/demo/json/", &ret)
 	if err != nil {
 		return
 	}
 	fmt.Println(ret.Code)
+	// Output:
+	// 0
+}
+
+func ExamplePostURL() {
+	s, err := my.PostURL("http://54600.net/demo/post/", "name=zs&age=20")
+	if err != nil {
+		return
+	}
+	fmt.Println(string(s))
+	// Output:
+	// name:zs
+	// age:20
 }
