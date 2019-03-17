9f846e2500d77a30b5e559f9f4a32a3e43b5bf7f
zs5460 <zs5460@gmail.com>
2019-03-17 21:26:54 +0800

    style

--- convert.go
+++ convert.go
@@ -12,23 +12,23 @@ func CStr(i int) string {
 }
 
 // CInt returns the result of strconv.Atoi.
-func CInt(s string) (ret int) {
+func CInt(s string) int {
 	ret, err := strconv.Atoi(s)
 	if err != nil {
 		return 0
 	}
-	return
+	return ret
 }
 
 // CDate returns the result of time.ParseInLocation.
-func CDate(s string) (ret time.Time) {
+func CDate(s string) time.Time {
 	ret, err := time.ParseInLocation("2006-1-2 15:04:05", s, time.Local)
 	if err != nil {
 		ret, _ = time.ParseInLocation("2006-01-02 15:04:05",
 			"1900-01-01 00:00:00",
 			time.Local)
 	}
-	return
+	return ret
 }
 
 func str2bytes(s string) []byte {
