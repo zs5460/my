b22bb6ae271dce49908657ee3d0f8b03702f0d5f
zs5460 <zs5460@gmail.com>
2019-03-19 14:02:43 +0800

    update:CDate

--- convert.go
+++ convert.go
@@ -24,9 +24,11 @@ func CInt(s string) int {
 func CDate(s string) time.Time {
 	ret, err := time.ParseInLocation("2006-1-2 15:04:05", s, time.Local)
 	if err != nil {
-		ret, _ = time.ParseInLocation("2006-01-02 15:04:05",
-			"1900-01-01 00:00:00",
-			time.Local)
+		ret, err := time.ParseInLocation("2006-1-2", s, time.Local)
+		if err != nil {
+			ret, _ = time.ParseInLocation("2006-01-02", "1900-01-01", time.Local)
+		}
+		return ret
 	}
 	return ret
 }
