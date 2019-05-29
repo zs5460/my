07cc59444cf5c4985c24f1b4f7fae10874920026
zs5460 <zs5460@gmail.com>
2019-05-29 21:47:14 +0800

    add examples

--- example_test.go
+++ example_test.go
@@ -2,10 +2,36 @@ package my_test
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"time"
 
 	"github.com/zs5460/my"
 )
+
+func ExampleLoadXMLConfig() {
+	type config struct {
+		AppName string `xml:"appname"`
+		Version string `xml:"version"`
+	}
+	var c config
+	my.LoadXMLConfig("testdata/config.xml", &c)
+	fmt.Println(c.AppName)
+	fmt.Println(c.Version)
+	// Output:
+	// DEMO
+	// 1.0.0
+}
+
+func ExampleIsEmpty() {
+	var s string
+	fmt.Println(my.IsEmpty(s))
+	s = "zs5460"
+	fmt.Println(my.IsEmpty(s))
+	// Output:
+	// true
+	// false
+}
 
 func ExampleGetURL() {
 	s, err := my.GetURL("http://54600.net/demo/ping")
@@ -45,6 +71,19 @@ func ExamplePostURL() {
 	// age:20
 }
 
+func ExampleDownloadFile() {
+	source := "https://avatars3.githubusercontent.com/u/1711957"
+	localfile := "testdata/avatar.jpg"
+	err := my.DownloadFile(source, localfile)
+	if err != nil {
+		log.Println(err)
+	}
+	os.Remove(localfile) //clean up
+	fmt.Println("OK")
+	// Output:
+	// OK
+}
+
 func ExampleFormatDateTime() {
 	birthday := time.Date(1982, time.February, 11, 20, 13, 14, 0, time.Local)
 	fmt.Println(my.FormatDateTime(birthday, 0))
@@ -72,30 +111,6 @@ func ExampleLoadJSONConfig() {
 	// Output:
 	// DEMO
 	// 1.0.0
-}
-
-func ExampleLoadXMLConfig() {
-	type config struct {
-		AppName string `xml:"appname"`
-		Version string `xml:"version"`
-	}
-	var c config
-	my.LoadXMLConfig("testdata/config.xml", &c)
-	fmt.Println(c.AppName)
-	fmt.Println(c.Version)
-	// Output:
-	// DEMO
-	// 1.0.0
-}
-
-func ExampleIsEmpty() {
-	var s string
-	fmt.Println(my.IsEmpty(s))
-	s = "zs5460"
-	fmt.Println(my.IsEmpty(s))
-	// Output:
-	// true
-	// false
 }
 
 func ExampleLeft() {
@@ -145,4 +160,22 @@ func ExampleSpace() {
 	// Output:
 	// a     b
 	// 1234567
+}
+
+func ExampleTest() {
+	// pattern can be english,chinese,idcard,email,qq,zip,phone,mobile,url,username,password.
+	s := "zs5460"
+	fmt.Println(my.Test(s, "english"))
+	s = "zhousong"
+	fmt.Println(my.Test(s, "english"))
+	s = "zs5460.gmail.com"
+	fmt.Println(my.Test(s, "email"))
+	s = "zs5460@gmail.com"
+	fmt.Println(my.Test(s, "email"))
+
+	// Output:
+	// false
+	// true
+	// false
+	// true
 }
